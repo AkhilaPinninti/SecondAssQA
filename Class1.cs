272e1eff3aeaad0f7c00a4247743d934eb526763
AkhilaPinninti <pinnintiakhilarao@gmail.com>
2021-10-06 13:45:55 +0530

    first commit

---
+++ Class1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstQa
+{
+    public class Rectangle
+    {
+        private int length;
+        private int width;
+        private int area;
+        private int perimeter;
+        public Rectangle()
+        {
+            length = 1;
+            width = 1;
+
+        }
+        public Rectangle(int l, int w)
+        {
+            length = l;
+            width = w;
+        }
+        public int SetLength(int l) => length = l;
+        public int SetWidth(int w) => width = w;
+        public int GetLength()
+        {
+            return length;
+        }
+        public int GetWidth()
+        {
+            return width;
+        }
+        public int GetArea()
+        {
+            return area = length * width;
+        }
+        public int GetPerimeter()
+        {
+            return perimeter = (length + width) * 2;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace firstQa
+{
+    class Program
+    {
+      
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the length:");
+            int length = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the width:");
+            int width = Convert.ToInt32(Console.ReadLine());
+
+            Rectangle r = new Rectangle(length,width);
+            Console.WriteLine("Area is:" + r.GetArea());
+            Console.WriteLine("Perimeter is:" + r.GetPerimeter());
+            Console.WriteLine("Rectangle of length{0} and width{1} has area{2} and parameter{3}", length, width);
+            Rectangle r1 = new Rectangle();
+            Console.WriteLine("Area is:" + r1.GetArea());
+            Console.WriteLine("Perimeter is:" + r1.GetPerimeter());
+            Console.WriteLine("Rectangle of default length{0} and width{1} has area{2} with parameter{3}", length, width);  
+        }
+
+    }
+}
